58a3c194ba11e6756644a689e5ff0947031383bf
lochlynlaskowski <lochlyn.laskowski@gmail.com>
2022-06-27 16:37:26 -0500

    save point

---
+++ prepare.py
@@ -0,0 +1,16 @@
+def prepare_sales_data(df):
+    df['sale_date'] = pd.to_datetime(df['sale_date'])
+    df = df.set_index('sale_date')
+    df['month'] = df.index.strftime('%m-%b')
+    df['day'] = df.index.strftime('%w-%a')
+    df['sales_total'] = df.sale_amount * df.item_price
+    return df
+
+
+def prepare_opsd_data(df):
+    df['Date'] = pd.to_datetime(df['Date'])
+    df = df.set_index('Date')
+    df['month'] = df.index.strftime('%m-%b')
+    df['year'] = df.index.year
+    df = df.fillna(0)
+    return df
